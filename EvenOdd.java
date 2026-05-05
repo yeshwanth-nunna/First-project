d8972cbf376ee767e2596ee4a202c6c1bd5552e6
yeshwanth-nunna <yeshwanthnunna1@gmail.com>
2026-05-05 22:27:50 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
