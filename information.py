cb645052c83a07a3d6f1683827b340709640a038
yeshwanth-nunna <yeshwanthnunna1@gmail.com>
2026-05-06 21:16:14 +0530

    Add user input and print statements

---
+++ information.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+place = input("Enter your state: ")
+favorite_subject = input("Enter your favorite subject: ")
+
+print("My name is", name)
+print("I am from", place)
+print("My favorite subject is", favorite_subject)
